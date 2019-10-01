c8fc127fc679f93d6f9f2243fb84b3f2a41cd9ff
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-01 19:11:00 -0400

    MVP of game complete

--- finishLine/finishLine/Program.cs
+++ finishLine/finishLine/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            var game = new FinishLine(1, new string[] { "player1" });
+            var game = new FinishLine(2, new string[] { "Cliff", "Joe" });
             game.PlayGame();
         }
     }
