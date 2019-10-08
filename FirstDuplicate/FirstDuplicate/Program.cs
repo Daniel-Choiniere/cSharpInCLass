0719760ef71d5c669c5a1912c73f4f082bf8636c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-08 15:38:11 -0400

    refactored code

--- FirstDuplicate/FirstDuplicate/Program.cs
+++ FirstDuplicate/FirstDuplicate/Program.cs
@@ -8,17 +8,12 @@ class MainClass
         List<int> NumbersInList = new List<int>();
         List<int> NumToCheck = new List<int>();
 
-        foreach (int number in array)
-        {
-            NumbersInList.Add(number);
-        }
+        foreach (int number in array) NumbersInList.Add(number);
+        
 
         foreach (int item in NumbersInList)
         {
-            if (NumToCheck.IndexOf(item) != -1)
-            {
-                return item;
-            }
+            if (NumToCheck.IndexOf(item) != -1) return item;
             NumToCheck.Add(item);
         }
         return -1;
