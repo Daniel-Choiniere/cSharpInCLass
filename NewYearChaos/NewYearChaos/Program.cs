27ae8a254974228ac154212cfdf2899df73afa42
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-09 08:25:44 -0400

    extracted build list method

--- NewYearChaos/NewYearChaos/Program.cs
+++ NewYearChaos/NewYearChaos/Program.cs
@@ -5,35 +5,61 @@ namespace NewYearChaos
 {
     class MainClass
     {
+        //public static bool IsSorted(List<int> list)
+        //{
+        //    for (int i = 1; i < list.Count; i++)
+        //    {
+        //        if (list[i - 1] > list[i])
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return true;
+        //}
+
         static int MinimumBribes(int[] q)
         {
+            List<int> LineQueue = BuildList(q);
 
-            List<int> Queue = new List<int>();
             int BribeCounter = 0;
 
-            foreach (int item in q)
+            for (int j = 0; j < LineQueue.Count; j++)
             {
-                Queue.Add(item);
-            }
-
-            for (int i = 1; i < Queue.Count; i++)
-            {
-                //add in logic to check if swaps are more than 2
-
-                //our loop will not go back through the array to check for swapping again
-                if (Queue[i] < Queue[i - 1])
+                for (int i = 1; i < LineQueue.Count; i++)
                 {
-                    BribeCounter++;
-                    Console.WriteLine(BribeCounter);
-                    Queue[i] = Queue[i - 1];
+                    //add in logic to check if swaps are more than 2
+
+                    if (LineQueue[i] < LineQueue[i - 1])
+                    {
+                        BribeCounter++;
+
+                        int temp;
+                        temp = LineQueue[i - 1];
+                        LineQueue[i - 1] = LineQueue[i];
+                        LineQueue[i] = temp;
+                    }
                 }
             }
             return BribeCounter;
         }
+
+        private static List<int> BuildList(int[] q)
+        {
+            List<int> Queue = new List<int>();
+
+            foreach (int item in q)
+                Queue.Add(item);
+
+            return Queue;
+        }
+
         public static void Main(string[] args)
         {
-            int[] array = { 1, 2, 5, 3, 7, 8, 6, 4 };
-            Console.WriteLine(MinimumBribes(array));
+            int[] array = { 1, 2, 5, 3, 4, 7, 8, 6 };
+
+            int totalNumberOfBribes = MinimumBribes(array);
+
+            Console.WriteLine(totalNumberOfBribes);
         }
     }
 }
