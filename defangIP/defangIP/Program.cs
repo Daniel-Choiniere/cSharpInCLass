88e9cc941e963bf72ceb9e18afcb3cb3aeeaa059
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-30 07:54:43 -0400

    working, very efficent solution to defang an IP. passes all test cases

---
+++ defangIP/defangIP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace defangIP
+{
+    class Program
+    {
+        static string DefangIPaddr(string address) 
+        {
+            return address.Replace(".", "[.]");
+        
+        }
+        
+        public static void Main(string[] args)
+        {
+            string address = "1.1.1.1";
+            Console.WriteLine(DefangIPaddr(address));
+        }
+    }
+}
