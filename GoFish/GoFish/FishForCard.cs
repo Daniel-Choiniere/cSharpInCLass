47683e7d47ee929617ffb46a5438759b45fb99a4
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-12-05 09:00:52 -0500

    selecting and removing a card from the AIs hand

--- GoFish/GoFish/FishForCard.cs
+++ GoFish/GoFish/FishForCard.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using GoFish.CardDeck;
 
 namespace GoFish
 {
@@ -20,13 +22,21 @@ namespace GoFish
             Console.Write("What card would you like to try for a match: ");
             var fishCard = Console.ReadLine();
 //            Console.WriteLine(fishCard);
-            foreach (var item in AI.Cards)
+            
+//            Need to make a new list to be able to remove from the original AI card list
+            List<Card> cardCopy = new List<Card>(AI.Cards);
+            
+            foreach (var item in cardCopy)
             {
                 string cardDescrip = item.Number + " " +  item.Suit;
                 if (cardDescrip == fishCard)
                 {
-                    Console.WriteLine("Holla At cha Boy!!!");
+                    AI.Cards.Remove(item);
                 }
+            }
+            foreach (var item in AI.Cards)
+            {
+                Console.WriteLine("Deck Two: {0} {1}",  item.Number, item.Suit);
             }
         }
     }
